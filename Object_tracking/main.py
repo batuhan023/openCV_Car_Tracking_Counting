49a094e1ca264869bbcc031286af938fd3b266e8
Batuhan Yavuz <92747705+batuhan023@users.noreply.github.com>
2022-04-14 15:38:10 +0300

    Update main.py

--- Object_tracking/main.py
+++ Object_tracking/main.py
@@ -7,7 +7,7 @@ tracker = EuclideanDistTracker()
 cap = cv2.VideoCapture("highway_Trim.mp4")
 # object detection from stable camera
 
-object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=400)
 
 while True:
     ret, frame = cap.read()
@@ -15,21 +15,21 @@ while True:
     # extract region of interest
     roi = frame[370: 720, 90: 590]
 
+    border = cv2.line(frame, (525, 630), (20, 630), (0, 0, 255), 1)
 # object detection
 
     mask = object_detector.apply(roi)
-    _, mask = cv2.threshold(mask, 245, 255, cv2.THRESH_BINARY)  # for remove shadow but doesn't work my video
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)  # for remove shadow but doesn't work my video
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     detections = []
 
     for cnt in contours:
         # calculate area and remove small area
         area = cv2.contourArea(cnt)
-        if  area > 1000:
+        if area > 2000:
             x, y, w, h = cv2.boundingRect(cnt)
-
             detections.append([x, y, w, h])
-           # cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
+         #cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
 
 # object tracking
 
